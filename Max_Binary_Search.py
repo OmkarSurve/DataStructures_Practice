279e28469d73363e5bf55f15c128687cd5fe60cb
OMKAR SURVE <omkarsurve25@gmail.com>
2022-05-29 14:45:09 +0530

    Find maximum element in an array
    Array can be continuously increasing, decreasing or first increasing and then decreasing

--- Max_Binary_Search.py
+++ Max_Binary_Search.py
@@ -1,9 +1,4 @@
 """
-One array of integers is given as an input ,which is initially increasing and then decreasing or it can be only increasing or decreasing , 
-you need to find the maximum value in the array in O(Log n) Time complexity and O(1) Space Complexity Asked in : AmazonMicrosoftUber
-"""
-
-"""
 Input: array[] = {3, 5,15, 50, 11, 10, 8, 6}
 {3, 5, 20, 15, 14, 13, 12, 11}
 {2, 3, 5, 7, 9, 10, 8}
@@ -38,5 +33,8 @@ if __name__ == '__main__':
     high = len(ar) - 1
 
     print (max_bin(ar, low, high))
+
+
+
 
 
