ff5876456a697b50508804d7fec2a8673269b54c
OMKAR SURVE <omkarsurve25@gmail.com>
2022-05-29 13:26:16 +0530

    Finding Maximum using Binary Search

---
+++ Max_Binary_Search.py
@@ -0,0 +1,40 @@
+"""
+Input: array[] = {3, 5,15, 50, 11, 10, 8, 6}
+{3, 5, 20, 15, 14, 13, 12, 11}
+{2, 3, 5, 7, 9, 10, 8}
+"""
+
+
+def max_bin(ar, low, high):
+
+    if ar[low] == ar[high]:
+        return ar[low]
+
+    if high == low + 1:
+        if ar[high] > ar[low]:
+            return ar[high]
+
+        return ar[low]
+
+    mid = low + (high-low)//2
+
+    if (ar[mid-1] < ar[mid]) and (ar[mid] > ar[mid+1]):
+        return max_bin(ar, low, mid)
+    elif ar[mid-1] < ar[mid] < ar[mid+1]:
+        return max_bin(ar, mid+1, high)
+    else:
+        return max_bin(ar, low, mid-1)
+
+
+if __name__ == '__main__':
+
+    ar = [14, 10, 9, 8, 6, 4, 2]
+    low = 0
+    high = len(ar) - 1
+
+    print (max_bin(ar, low, high))
+
+
+
+
+
