16808a2f245d001dd5eece5c4deb699dbbfdf8c0
OMKAR SURVE <omkarsurve25@gmail.com>
2022-06-19 22:20:52 +0530

    In-place matrix rotation by 90 degree

---
+++ matrix_rotation.py
@@ -0,0 +1,22 @@
+matrix = [[1, 2, 3, 4],
+          [5, 6, 7, 8],
+          [9, 10, 11, 12],
+          [13, 14, 15, 16],
+          ]
+n = 4
+
+
+def matrix_rotation(matrix, n):
+
+    for i in range(0, 2):
+        for j in range(i, n-i-1):
+            t = matrix[i][j]
+            matrix[i][j] = matrix[n - 1 - j][i]    # j = n - x
+            matrix[n - 1 - j][i] = matrix[n - 1 - i][n - 1 - j]  # i = n - 1 - x
+            matrix[n - 1 - i][n - 1 - j] = matrix[j][n - 1 - i]   # n - 1 - j = n - 1 - x  n - 1 - n + 1 + j
+            matrix[j][n - 1 - i] = t
+
+    return matrix
+
+
+print (matrix_rotation(matrix, n))
