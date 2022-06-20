57504ce5962c0d5623933a676ab10ee51a88b259
OMKAR SURVE <omkarsurve25@gmail.com>
2022-06-20 11:02:03 +0530

    Added comments for understanding

--- matrix_rotation.py
+++ matrix_rotation.py
@@ -10,6 +10,8 @@ def matrix_rotation(matrix, n):
 
     for i in range(0, 2):
         for j in range(i, n-i-1):
+            # At every step, we apply logic, column of source is row of destination and
+            # column of destination is n - 1 - row of source
             t = matrix[i][j]
             matrix[i][j] = matrix[n - 1 - j][i]    # j = n - x
             matrix[n - 1 - j][i] = matrix[n - 1 - i][n - 1 - j]  # i = n - 1 - x
